b0ab585ce61dc26f2c9cdb5ad5affb3135ce2816
Christiaan Rakowski <cs.rakowski@gmail.com>
2018-01-06 22:20:02 +0100

    Copied the benchmarks test over into a console project

---
+++ tests/CSRakowski.ParallelAsync.Benchmarks/TestResult.cs
@@ -0,0 +1,23 @@
+﻿namespace CSRakowski.Parallel.Benchmarks
+{
+    public class TestResult
+    {
+        public TestResult(int runId, int numberOfJobs, int numberOfResults, double elapsedMilliseconds)
+        {
+            RunID = (runId == -1) ? "Average" : runId.ToString();
+            NumberOfJobs = numberOfJobs;
+            NumberOfResults = numberOfResults;
+            ElapsedMilliseconds = elapsedMilliseconds;
+        }
+
+        public string RunID { get; }
+
+        public int NumberOfJobs { get; }
+
+        public int NumberOfResults { get; }
+
+        public double ElapsedMilliseconds { get; }
+
+        public double MessagesPerMillisecond => NumberOfResults / ElapsedMilliseconds;
+    }
+}
