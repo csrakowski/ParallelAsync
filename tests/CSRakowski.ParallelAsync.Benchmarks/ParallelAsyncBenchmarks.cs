ce78656a5e7f37938ff0fbe014c72987ae83a9a0
Christiaan Rakowski <cs.rakowski@gmail.com>
2018-01-22 10:25:03 +0100

    Quick first gist implementing BenchmarkDotNet

---
+++ tests/CSRakowski.ParallelAsync.Benchmarks/ParallelAsyncBenchmarks.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Attributes;
+
+namespace CSRakowski.Parallel.Benchmarks
+{
+    public class ParallelAsyncBenchmarks
+    {
+        private const int NumberOfItemsInCollection = 10000;
+
+        private readonly List<int> InputNumbers;
+
+        public ParallelAsyncBenchmarks()
+        {
+            InputNumbers = Enumerable.Range(0, NumberOfItemsInCollection).ToList();
+        }
+
+        #region Benchmarks: JustAddOne
+
+        [Benchmark]
+        public async Task Unbatched_AsOrdered()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 1, outOfOrder: false);
+        }
+
+        [Benchmark]
+        public async Task Unbatched_OutOfOrder()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 1, outOfOrder: true);
+        }
+
+        [Benchmark]
+        public async Task MaxBatch_2_AsOrdered()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 2, outOfOrder: false);
+        }
+
+        [Benchmark]
+        public async Task MaxBatch_2_OutOfOrder()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 2, outOfOrder: true);
+        }
+
+        [Benchmark]
+        public async Task MaxBatch_4_AsOrdered()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 4, outOfOrder: false);
+        }
+
+        [Benchmark]
+        public async Task MaxBatch_4_OutOfOrder()
+        {
+            await Run_JustAddOne_With(maxBatchSize: 4, outOfOrder: true);
+        }
+
+        #endregion Benchmarks: JustAddOne
+
+
+        #region Helpers
+
+        private async Task Run_JustAddOne_With(int maxBatchSize, bool outOfOrder)
+        {
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, JustAddOne, maxBatchSize, outOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        private async Task Run_JustAddOneWithCancellation_With(int maxBatchSize, bool outOfOrder)
+        {
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, JustAddOneWithCancellation, maxBatchSize, outOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        private async Task Run_ReturnTaskCompletedTask_With(int maxBatchSize, bool outOfOrder)
+        {
+            await ParallelAsync.ForEachAsync(InputNumbers, ReturnTaskCompletedTask, maxBatchSize, outOfOrder, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        private async Task Run_ReturnTaskCompletedTaskWithCancellation_With(int maxBatchSize, bool outOfOrder)
+        {
+            await ParallelAsync.ForEachAsync(InputNumbers, ReturnTaskCompletedTaskWithCancellation, maxBatchSize, outOfOrder, CancellationToken.None).ConfigureAwait(false);
+        }
+
+        #endregion Helpers
+
+        #region Delegates
+
+        private static Task<int> JustAddOne(int number)
+        {
+            return Task.FromResult(number + 1);
+        }
+
+        private static Task<int> JustAddOneWithCancellation(int number, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(number + 1);
+        }
+
+        private static Task ReturnTaskCompletedTask(int number)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static Task ReturnTaskCompletedTaskWithCancellation(int number, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion Delegates
+
+    }
+}
--- tests/CSRakowski.ParallelAsync.Benchmarks/Program.cs
+++ tests/CSRakowski.ParallelAsync.Benchmarks/Program.cs
@@ -5,86 +5,19 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Attributes;
 
 namespace CSRakowski.Parallel.Benchmarks
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             // TODO: Rewrite to Benchmark.NET
-            await Performance_Degradation_Run(0, 10000, true);
-            await Performance_Degradation_Run(0, 10000, false);
-            await Performance_Degradation_Run(1, 10000, true);
-            await Performance_Degradation_Run(1, 10000, false);
-            await Performance_Degradation_Run(2, 10000, true);
-            await Performance_Degradation_Run(2, 10000, false);
-            await Performance_Degradation_Run(8, 10000, true);
-            await Performance_Degradation_Run(8, 10000, false);
-            await Performance_Degradation_Run(128, 10000, true);
-            await Performance_Degradation_Run(128, 10000, false);
-        }
-
-        private static async Task Performance_Degradation_Run(int maxBatchSize, int numberOfItemsInCollection, bool outOfOrder)
-        {
-            const int numberOfRuns = 5;
-
-            var numbers = Enumerable.Range(0, numberOfItemsInCollection).ToList();
-
-            #region Pre-benchmark JIT run
-
-            var defaultColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            Console.WriteLine("Single runs to pre-jit it all");
-
-            await ParallelAsync.ForEachAsync(numbers, JustAddOne, maxBatchSize, outOfOrder, numberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
-
-            Console.ForegroundColor = defaultColor;
-
-            Console.WriteLine("Pre-jit done!\nRunning.");
-
-            #endregion Pre-benchmark JIT run
-
-            var results = new List<TestResult>(numberOfRuns + 1);
-
-            for (int runCount = 0; runCount < numberOfRuns; runCount++)
-            {
-                var timer = Stopwatch.StartNew();
-
-                var batchResults = await ParallelAsync.ForEachAsync(numbers, JustAddOne, maxBatchSize, outOfOrder, numberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
-
-                timer.Stop();
-                var resultCount = batchResults.Count();
-                var elapsed = timer.Elapsed.TotalMilliseconds;
-
-                var testResult = new TestResult(runCount, numberOfItemsInCollection, resultCount, elapsed);
-                results.Add(testResult);
-            }
-
-            var totalElapsed = results.Sum(r => r.ElapsedMilliseconds);
-            var totalResults = results.Sum(r => r.NumberOfResults);
-            var average = new TestResult(-1, numberOfItemsInCollection, totalResults, totalElapsed);
-            results.Add(average);
-
-            var sb = new StringBuilder($"Results {nameof(ParallelAsync)} ({nameof(maxBatchSize)}:{maxBatchSize}, {nameof(numberOfItemsInCollection)}:{numberOfItemsInCollection}, {nameof(outOfOrder)}:{outOfOrder})")
-                            .Append("\n\nRun;NumberOfMessages;Elapsed (ms);Throughput (messages/ms);\n");
-
-            foreach (var result in results)
-            {
-                sb.Append(result.RunID).Append(";")
-                    .Append(result.NumberOfResults).Append(";")
-                    .Append(result.ElapsedMilliseconds).Append(";")
-                    .Append(result.MessagesPerMillisecond).Append(";\n");
-            }
-
-            var resultCsv = sb.ToString();
-            Console.WriteLine(resultCsv);
-        }
-
-        private static Task<int> JustAddOne(int number)
-        {
-            return Task.FromResult(number + 1);
+            var summary = BenchmarkRunner.Run<ParallelAsyncBenchmarks>();
         }
     }
 }
