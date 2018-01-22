42845a91aa0f6521d050ad15319a55fe1be95639
Christiaan Rakowski <cs.rakowski@gmail.com>
2018-01-22 11:00:42 +0100

    * Found out there is a Params attribute, so used that. * Added new delegates that have a Task.Delay in them, to balance the overhead of the OutOfOrder logic a bit

--- tests/CSRakowski.ParallelAsync.Benchmarks/ParallelAsyncBenchmarks.cs
+++ tests/CSRakowski.ParallelAsync.Benchmarks/ParallelAsyncBenchmarks.cs
@@ -15,6 +15,7 @@ namespace CSRakowski.Parallel.Benchmarks
     public class ParallelAsyncBenchmarks
     {
         private const int NumberOfItemsInCollection = 10000;
+        private const int DelayTime = 250;
 
         private readonly List<int> InputNumbers;
 
@@ -23,210 +24,103 @@ namespace CSRakowski.Parallel.Benchmarks
             InputNumbers = Enumerable.Range(0, NumberOfItemsInCollection).ToList();
         }
 
-        #region Benchmarks: JustAddOne
+        [Params(1, 2, 4, 8)]
+        public int MaxBatchSize { get; set; }
+
+        [Params(false, true)]
+        public bool AllowOutOfOrder { get; set; }
+
 
         [Benchmark]
-        public async Task Run_JustAddOne_Unbatched_AsOrdered()
+        public async Task JustAddOne()
         {
-            await Run_JustAddOne_With(maxBatchSize: 1, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_JustAddOne_Unbatched_OutOfOrder()
-        {
-            await Run_JustAddOne_With(maxBatchSize: 1, outOfOrder: true);
-        }
-
-        [Benchmark]
-        public async Task Run_JustAddOne_MaxBatch_2_AsOrdered()
-        {
-            await Run_JustAddOne_With(maxBatchSize: 2, outOfOrder: false);
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, Func_JustAddOne, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOne_MaxBatch_2_OutOfOrder()
+        public async Task JustAddOne_WithCancellation()
         {
-            await Run_JustAddOne_With(maxBatchSize: 2, outOfOrder: true);
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, Func_JustAddOne_WithCancellation, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOne_MaxBatch_4_AsOrdered()
+        public async Task ReturnTaskCompletedTask()
         {
-            await Run_JustAddOne_With(maxBatchSize: 4, outOfOrder: false);
+            await ParallelAsync.ForEachAsync(InputNumbers, Func_ReturnTaskCompletedTask, MaxBatchSize, AllowOutOfOrder, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOne_MaxBatch_4_OutOfOrder()
+        public async Task ReturnTaskCompletedTask_WithCancellation()
         {
-            await Run_JustAddOne_With(maxBatchSize: 4, outOfOrder: true);
-        }
-
-        #endregion Benchmarks: JustAddOne
-
-        #region Benchmarks: JustAddOneWithCancellation
-
-        [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_Unbatched_AsOrdered()
-        {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 1, outOfOrder: false);
+            await ParallelAsync.ForEachAsync(InputNumbers, Func_ReturnTaskCompletedTask_WithCancellation, MaxBatchSize, AllowOutOfOrder, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_Unbatched_OutOfOrder()
+        public async Task JustAddOne_WithDelay()
         {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 1, outOfOrder: true);
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, Func_JustAddOne_WithDelay, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_MaxBatch_2_AsOrdered()
+        public async Task JustAddOne_WithCancellation_WithDelay()
         {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 2, outOfOrder: false);
+            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, Func_JustAddOne_WithCancellation_WithDelay, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_MaxBatch_2_OutOfOrder()
+        public async Task ReturnTaskCompletedTask_WithDelay()
         {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 2, outOfOrder: true);
+            await ParallelAsync.ForEachAsync(InputNumbers, Func_ReturnTaskCompletedTask_WithDelay, MaxBatchSize, AllowOutOfOrder, CancellationToken.None).ConfigureAwait(false);
         }
 
         [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_MaxBatch_4_AsOrdered()
+        public async Task ReturnTaskCompletedTask_WithCancellation_WithDelay()
         {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 4, outOfOrder: false);
+            await ParallelAsync.ForEachAsync(InputNumbers, Func_ReturnTaskCompletedTask_WithCancellation_WithDelay, MaxBatchSize, AllowOutOfOrder, CancellationToken.None).ConfigureAwait(false);
         }
-
-        [Benchmark]
-        public async Task Run_JustAddOneWithCancellation_MaxBatch_4_OutOfOrder()
-        {
-            await Run_JustAddOneWithCancellation_With(maxBatchSize: 4, outOfOrder: true);
-        }
-
-        #endregion Benchmarks: JustAddOneWithCancellation
-
-        #region Benchmarks: ReturnTaskCompletedTask
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_Unbatched_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 1, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_Unbatched_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 1, outOfOrder: true);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_MaxBatch_2_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 2, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_MaxBatch_2_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 2, outOfOrder: true);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_MaxBatch_4_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 4, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTask_MaxBatch_4_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTask_With(maxBatchSize: 4, outOfOrder: true);
-        }
-
-        #endregion Benchmarks: ReturnTaskCompletedTask
-
-        #region Benchmarks: ReturnTaskCompletedTaskWithCancellation
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_Unbatched_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 1, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_Unbatched_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 1, outOfOrder: true);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_MaxBatch_2_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 2, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_MaxBatch_2_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 2, outOfOrder: true);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_MaxBatch_4_AsOrdered()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 4, outOfOrder: false);
-        }
-
-        [Benchmark]
-        public async Task Run_ReturnTaskCompletedTaskWithCancellation_MaxBatch_4_OutOfOrder()
-        {
-            await Run_ReturnTaskCompletedTaskWithCancellation_With(maxBatchSize: 4, outOfOrder: true);
-        }
-
-        #endregion Benchmarks: ReturnTaskCompletedTaskWithCancellation
-
-        #region Helpers
-
-        private async Task Run_JustAddOne_With(int maxBatchSize, bool outOfOrder)
-        {
-            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, JustAddOne, maxBatchSize, outOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        private async Task Run_JustAddOneWithCancellation_With(int maxBatchSize, bool outOfOrder)
-        {
-            var batchResults = await ParallelAsync.ForEachAsync(InputNumbers, JustAddOneWithCancellation, maxBatchSize, outOfOrder, NumberOfItemsInCollection, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        private async Task Run_ReturnTaskCompletedTask_With(int maxBatchSize, bool outOfOrder)
-        {
-            await ParallelAsync.ForEachAsync(InputNumbers, ReturnTaskCompletedTask, maxBatchSize, outOfOrder, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        private async Task Run_ReturnTaskCompletedTaskWithCancellation_With(int maxBatchSize, bool outOfOrder)
-        {
-            await ParallelAsync.ForEachAsync(InputNumbers, ReturnTaskCompletedTaskWithCancellation, maxBatchSize, outOfOrder, CancellationToken.None).ConfigureAwait(false);
-        }
-
-        #endregion Helpers
 
         #region Delegates
 
-        private static Task<int> JustAddOne(int number)
+        private static Task<int> Func_JustAddOne(int number)
         {
             return Task.FromResult(number + 1);
         }
 
-        private static Task<int> JustAddOneWithCancellation(int number, CancellationToken cancellationToken)
+        private static Task<int> Func_JustAddOne_WithCancellation(int number, CancellationToken cancellationToken)
         {
             return Task.FromResult(number + 1);
         }
 
-        private static Task ReturnTaskCompletedTask(int number)
+        private static Task Func_ReturnTaskCompletedTask(int number)
         {
             return Task.CompletedTask;
         }
 
-        private static Task ReturnTaskCompletedTaskWithCancellation(int number, CancellationToken cancellationToken)
+        private static Task Func_ReturnTaskCompletedTask_WithCancellation(int number, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
+        }
+
+        private static async Task<int> Func_JustAddOne_WithDelay(int number)
+        {
+            await Task.Delay(DelayTime);
+            return (number + 1);
+        }
+
+        private static async Task<int> Func_JustAddOne_WithCancellation_WithDelay(int number, CancellationToken cancellationToken)
+        {
+            await Task.Delay(DelayTime);
+            return (number + 1);
+        }
+
+        private static Task Func_ReturnTaskCompletedTask_WithDelay(int number)
+        {
+            return Task.Delay(DelayTime);
+        }
+
+        private static Task Func_ReturnTaskCompletedTask_WithCancellation_WithDelay(int number, CancellationToken cancellationToken)
+        {
+            return Task.Delay(DelayTime);
         }
 
         #endregion Delegates
