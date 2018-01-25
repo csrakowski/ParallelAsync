c604ca68cb6886ef4ae7125e913a6297dd5f33b6
Christiaan Rakowski <cs.rakowski@gmail.com>
2018-01-25 14:45:50 +0100

    Added test IAsyncEnumerable implementation that just wraps a regular IEnumerable

---
+++ tests/CSRakowski.Parallel.Tests/Mocks and Helpers/TestAsyncEnumerable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSRakowski.Parallel.Tests.Helpers
+{
+    public static class TestAsyncEnumerableEx
+    {
+        public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this IEnumerable<T> enumerable)
+        {
+            return new TestAsyncEnumerable<T>(enumerable);
+        }
+    }
+
+    public class TestAsyncEnumerable<T> : IAsyncEnumerable<T>
+    {
+        private readonly IEnumerable<T> _enumerable;
+
+        internal TestAsyncEnumerable(IEnumerable<T> enumerable)
+        {
+            _enumerable = enumerable;
+        }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator() =>
+            new TestAsyncEnumerator<T>(_enumerable.GetEnumerator());
+    }
+
+    public struct TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        internal TestAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        public Task<bool> MoveNextAsync() =>
+            Task.FromResult(_enumerator.MoveNext());
+
+        public T Current => _enumerator.Current;
+
+        public Task DisposeAsync()
+        {
+            _enumerator.Dispose();
+            return TaskHelper.CompletedTask;
+        }
+    }
+}
