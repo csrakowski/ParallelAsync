b0b6a37314c8a4f9bdc57144fde40d66f08b2356
Christiaan Rakowski <cs.rakowski@gmail.com>
2018-01-27 17:19:18 +0100

    Tests pointed out that, as long as the estimatedResultSize is correct, the overhead from List<T> is marginal.

--- src/CSRakowski.Parallel/ParallelAsync.Unbatched.cs
+++ src/CSRakowski.Parallel/ParallelAsync.Unbatched.cs
@@ -58,7 +58,6 @@ namespace CSRakowski.Parallel
         {
             var collectionCount = collection.Count;
 
-            //TODO: Look into using Arrays directly, instead of relying on Adding using List (Perf/GC overhead)
             var result = ListHelpers.GetList<TResult>(collectionCount);
 
             long runId = ParallelAsyncEventSource.Log.GetRunId();
@@ -87,7 +86,6 @@ namespace CSRakowski.Parallel
 
         private static async Task<IEnumerable<TResult>> ForEachAsyncImplUnbatched_Array<TResult, TIn>(TIn[] collection, Func<TIn, CancellationToken, Task<TResult>> func, CancellationToken cancellationToken)
         {
-            //TODO: Look into using Arrays directly, instead of relying on Adding using List (Perf/GC overhead)
             var result = ListHelpers.GetList<TResult>(collection.Length);
 
             long runId = ParallelAsyncEventSource.Log.GetRunId();
