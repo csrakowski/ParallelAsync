3dfebe564293bf9f6c936b5aac4d6c6b492b4aad
Christiaan Rakowski <cs.rakowski@gmail.com>
2021-12-23 17:42:39 +0100

    Removed the WithDelay permutations

--- tests/CSRakowski.Parallel.Benchmarks/TestFunctions.cs
+++ tests/CSRakowski.Parallel.Benchmarks/TestFunctions.cs
@@ -9,8 +9,6 @@ namespace CSRakowski.Parallel.Benchmarks
 {
     public static class TestFunctions
     {
-        public static int DelayTime = 1;
-
         public static Task<int> JustAddOne(int number)
         {
             return Task.FromResult(number + 1);
@@ -19,17 +17,6 @@ namespace CSRakowski.Parallel.Benchmarks
         public static Task<int> JustAddOne_WithCancellationToken(int number, CancellationToken cancellationToken)
         {
             return Task.FromResult(number + 1);
-        }
-        public static async Task<int> JustAddOne_WithDelay(int number)
-        {
-            await Task.Delay(DelayTime).ConfigureAwait(false);
-            return (number + 1);
-        }
-
-        public static async Task<int> JustAddOne_WithCancellationToken_WithDelay(int number, CancellationToken cancellationToken)
-        {
-            await Task.Delay(DelayTime).ConfigureAwait(false);
-            return (number + 1);
         }
 
         public static Task ReturnCompletedTask(int number)
@@ -40,16 +27,6 @@ namespace CSRakowski.Parallel.Benchmarks
         public static Task ReturnCompletedTask_WithCancellationToken(int number, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
-        }
-
-        public static Task ReturnCompletedTask_WithDelay(int number)
-        {
-            return Task.Delay(DelayTime);
-        }
-
-        public static Task ReturnCompletedTask_WithCancellationToken_WithDelay(int number, CancellationToken cancellationToken)
-        {
-            return Task.Delay(DelayTime);
         }
     }
 }
