9368594e0f30bd22f7d20c6b1f131faf65c8bd0f
Christiaan Rakowski <cs.rakowski@gmail.com>
2021-12-27 12:44:40 +0100

    (Finally) Added first AsyncStream implementation #8

---
+++ src/CSRakowski.Parallel/ParallelAsync.AsyncStreams.cs
@@ -0,0 +1,294 @@
+﻿using CSRakowski.Parallel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSRakowski.Parallel
+{
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+
+    public static partial class ParallelAsync
+    {
+        #region IEnumerable<T>        
+
+        /// <summary>
+        /// Runs the specified async method for each item of the input collection in a parallel/batched manner.
+        /// </summary>
+        /// <typeparam name="TResult">The result item type</typeparam>
+        /// <typeparam name="TIn">The input item type</typeparam>
+        /// <param name="collection">The collection of items to use as input arguments</param>
+        /// <param name="func">The async method to run for each item</param>
+        /// <param name="maxBatchSize">The maximum batch size to allow. Use 0 to default to <c>Environment.ProcessorCount</c></param>
+        /// <param name="allowOutOfOrderProcessing">Boolean to allow out of order processing of input elements.</param>
+        /// <param name="estimatedResultSize">The estimated size of the result collection.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+        /// <returns>The results of the operations</returns>
+        /// <exception cref="ArgumentNullException">Thrown when either <paramref name="collection"/> or <paramref name="func"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="maxBatchSize"/> is a negative number.</exception>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="allowOutOfOrderProcessing" /> flag allows you to specify wether to allow the out of order processing mode.
+        /// This mode offers a performance improvement when the duration of each job varies, eg. due to network latency.
+        /// When each run takes roughly the same amount of time, running in out of order mode can/will actually perform worse.
+        /// As with all performance scenario's, do your own testing and pick what works for you.
+        /// </para>
+        /// <para>
+        /// The <paramref name="estimatedResultSize"/> value is used to determine the size of the result <see cref="List{T}"/> to account for.
+        /// The library will actually check if it can determine the size of <paramref name="collection"/>, without actually consuming it, using the <see cref="ListHelpers"/>.
+        /// If it is unable to determine a size there, it will fall back to the value you specified in <paramref name="estimatedResultSize"/>.
+        /// Setting this value to low, will mean a too small list will be allocated and you will have to pay a small performance hit for the resizing of the list during execution.
+        /// </para>
+        /// </remarks>
+        public static IAsyncEnumerable<TResult> ForEachAsyncStream<TResult, TIn>(IEnumerable<TIn> collection, Func<TIn, Task<TResult>> func, int maxBatchSize = 0, bool allowOutOfOrderProcessing = false, int estimatedResultSize = 0, CancellationToken cancellationToken = default)
+        {
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            var funcWithCancellationToken = WrapFunc(func);
+            return ForEachAsyncStream<TResult, TIn>(collection, funcWithCancellationToken, maxBatchSize, allowOutOfOrderProcessing, estimatedResultSize, cancellationToken);
+        }
+
+        /// <summary>
+        /// Runs the specified async method for each item of the input collection in a parallel/batched manner.
+        /// </summary>
+        /// <typeparam name="TResult">The result item type</typeparam>
+        /// <typeparam name="TIn">The input item type</typeparam>
+        /// <param name="collection">The collection of items to use as input arguments</param>
+        /// <param name="func">The async method to run for each item</param>
+        /// <param name="maxBatchSize">The maximum batch size to allow. Use 0 to default to <c>Environment.ProcessorCount</c></param>
+        /// <param name="allowOutOfOrderProcessing">Boolean to allow out of order processing of input elements.</param>
+        /// <param name="estimatedResultSize">The estimated size of the result collection.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+        /// <returns>The results of the operations</returns>
+        /// <exception cref="ArgumentNullException">Thrown when either <paramref name="collection"/> or <paramref name="func"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="maxBatchSize"/> is a negative number.</exception>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="allowOutOfOrderProcessing" /> flag allows you to specify wether to allow the out of order processing mode.
+        /// This mode offers a performance improvement when the duration of each job varies, eg. due to network latency.
+        /// When each run takes roughly the same amount of time, running in out of order mode can/will actually perform worse.
+        /// As with all performance scenario's, do your own testing and pick what works for you.
+        /// </para>
+        /// <para>
+        /// The <paramref name="estimatedResultSize"/> value is used to determine the size of the result <see cref="List{T}"/> to account for.
+        /// The library will actually check if it can determine the size of <paramref name="collection"/>, without actually consuming it, using the <see cref="ListHelpers"/>.
+        /// If it is unable to determine a size there, it will fall back to the value you specified in <paramref name="estimatedResultSize"/>.
+        /// Setting this value to low, will mean a too small list will be allocated and you will have to pay a small performance hit for the resizing of the list during execution.
+        /// </para>
+        /// </remarks>
+        public static IAsyncEnumerable<TResult> ForEachAsyncStream<TResult, TIn>(IEnumerable<TIn> collection, Func<TIn, CancellationToken, Task<TResult>> func, int maxBatchSize = 0, bool allowOutOfOrderProcessing = false, int estimatedResultSize = 0, CancellationToken cancellationToken = default)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            var maxBatchSizeToUse = DetermineBatchSizeToUse(maxBatchSize);
+
+            if (maxBatchSizeToUse == 1)
+            {
+                return ForEachAsyncStreamImplUnbatchedAsync<TResult, TIn>(collection, func, estimatedResultSize, cancellationToken);
+            }
+            else if (allowOutOfOrderProcessing)
+            {
+                throw new NotImplementedException();
+                //return ForEachAsyncStreamImplUnordered<TResult, TIn>(collection, func, maxBatchSizeToUse, estimatedResultSize, cancellationToken);
+            }
+            else
+            {
+                throw new NotImplementedException();
+                //return ForEachAsyncStreamImplOrdered<TResult, TIn>(collection, func, maxBatchSizeToUse, estimatedResultSize, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Implementation to run the specified async method for each item of the input collection in an unbatched manner.
+        /// </summary>
+        /// <typeparam name="TResult">The result item type</typeparam>
+        /// <typeparam name="TIn">The input item type</typeparam>
+        /// <param name="collection">The collection of items to use as input arguments</param>
+        /// <param name="func">The async method to run for each item</param>
+        /// <param name="estimatedResultSize">The estimated size of the result collection.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+        /// <returns>The results of the operations</returns>
+        private static async IAsyncEnumerable<TResult> ForEachAsyncStreamImplUnbatchedAsync<TResult, TIn>(IEnumerable<TIn> collection, Func<TIn, CancellationToken, Task<TResult>> func, int estimatedResultSize, CancellationToken cancellationToken)
+        {
+            long runId = ParallelAsyncEventSource.Log.GetRunId();
+            ParallelAsyncEventSource.Log.RunStart(runId, 1, false, estimatedResultSize);
+
+            int batchId = 0;
+            foreach (var element in collection)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                ParallelAsyncEventSource.Log.BatchStart(runId, batchId, 1);
+
+                var resultElement = await func(element, cancellationToken).ConfigureAwait(false);
+
+                ParallelAsyncEventSource.Log.BatchStop(runId, batchId);
+
+                batchId++;
+
+                yield return resultElement;
+            }
+
+            ParallelAsyncEventSource.Log.RunStop(runId);
+        }
+
+        #endregion IEnumerable<T>
+
+        #region IAsyncEnumerable<T>
+
+        /// <summary>
+        /// Runs the specified async method for each item of the input collection in a parallel/batched manner.
+        /// </summary>
+        /// <typeparam name="TResult">The result item type</typeparam>
+        /// <typeparam name="TIn">The input item type</typeparam>
+        /// <param name="collection">The collection of items to use as input arguments</param>
+        /// <param name="func">The async method to run for each item</param>
+        /// <param name="maxBatchSize">The maximum batch size to allow. Use 0 to default to <c>Environment.ProcessorCount</c></param>
+        /// <param name="allowOutOfOrderProcessing">Boolean to allow out of order processing of input elements.</param>
+        /// <param name="estimatedResultSize">The estimated size of the result collection.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+        /// <returns>The results of the operations</returns>
+        /// <exception cref="ArgumentNullException">Thrown when either <paramref name="collection"/> or <paramref name="func"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="maxBatchSize"/> is a negative number.</exception>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="allowOutOfOrderProcessing" /> flag allows you to specify wether to allow the out of order processing mode.
+        /// This mode offers a performance improvement when the duration of each job varies, eg. due to network latency.
+        /// When each run takes roughly the same amount of time, running in out of order mode can/will actually perform worse.
+        /// As with all performance scenario's, do your own testing and pick what works for you.
+        /// </para>
+        /// <para>
+        /// The <paramref name="estimatedResultSize"/> value is used to determine the size of the result <see cref="List{T}"/> to account for.
+        /// The library will actually check if it can determine the size of <paramref name="collection"/>, without actually consuming it, using the <see cref="ListHelpers"/>.
+        /// If it is unable to determine a size there, it will fall back to the value you specified in <paramref name="estimatedResultSize"/>.
+        /// Setting this value to low, will mean a too small list will be allocated and you will have to pay a small performance hit for the resizing of the list during execution.
+        /// </para>
+        /// </remarks>
+        public static IAsyncEnumerable<TResult> ForEachAsyncStream<TResult, TIn>(IAsyncEnumerable<TIn> collection, Func<TIn, Task<TResult>> func, int maxBatchSize = 0, bool allowOutOfOrderProcessing = false, int estimatedResultSize = 0, CancellationToken cancellationToken = default)
+        {
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            var funcWithCancellationToken = WrapFunc(func);
+            return ForEachAsyncStream<TResult, TIn>(collection, funcWithCancellationToken, maxBatchSize, allowOutOfOrderProcessing, estimatedResultSize, cancellationToken);
+        }
+
+        /// <summary>
+        /// Runs the specified async method for each item of the input collection in a parallel/batched manner.
+        /// </summary>
+        /// <typeparam name="TResult">The result item type</typeparam>
+        /// <typeparam name="TIn">The input item type</typeparam>
+        /// <param name="collection">The collection of items to use as input arguments</param>
+        /// <param name="func">The async method to run for each item</param>
+        /// <param name="maxBatchSize">The maximum batch size to allow. Use 0 to default to <c>Environment.ProcessorCount</c></param>
+        /// <param name="allowOutOfOrderProcessing">Boolean to allow out of order processing of input elements.</param>
+        /// <param name="estimatedResultSize">The estimated size of the result collection.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/></param>
+        /// <returns>The results of the operations</returns>
+        /// <exception cref="ArgumentNullException">Thrown when either <paramref name="collection"/> or <paramref name="func"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="maxBatchSize"/> is a negative number.</exception>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="allowOutOfOrderProcessing" /> flag allows you to specify wether to allow the out of order processing mode.
+        /// This mode offers a performance improvement when the duration of each job varies, eg. due to network latency.
+        /// When each run takes roughly the same amount of time, running in out of order mode can/will actually perform worse.
+        /// As with all performance scenario's, do your own testing and pick what works for you.
+        /// </para>
+        /// <para>
+        /// The <paramref name="estimatedResultSize"/> value is used to determine the size of the result <see cref="List{T}"/> to account for.
+        /// The library will actually check if it can determine the size of <paramref name="collection"/>, without actually consuming it, using the <see cref="ListHelpers"/>.
+        /// If it is unable to determine a size there, it will fall back to the value you specified in <paramref name="estimatedResultSize"/>.
+        /// Setting this value to low, will mean a too small list will be allocated and you will have to pay a small performance hit for the resizing of the list during execution.
+        /// </para>
+        /// </remarks>
+        public static IAsyncEnumerable<TResult> ForEachAsyncStream<TResult, TIn>(IAsyncEnumerable<TIn> collection, Func<TIn, CancellationToken, Task<TResult>> func, int maxBatchSize = 0, bool allowOutOfOrderProcessing = false, int estimatedResultSize = 0, CancellationToken cancellationToken = default)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            int maxBatchSizeToUse = DetermineBatchSizeToUse(maxBatchSize);
+
+            if (maxBatchSizeToUse == 1)
+            {
+                return ForEachAsyncStreamImplUnbatchedAsync<TResult, TIn>(collection, func, estimatedResultSize, cancellationToken);
+            }
+            else if (allowOutOfOrderProcessing)
+            {
+                throw new NotImplementedException();
+                //return ForEachAsyncStreamImplUnorderedAsync<TResult, TIn>(collection, func, maxBatchSizeToUse, estimatedResultSize, cancellationToken);
+            }
+            else
+            {
+                throw new NotImplementedException();
+                //return ForEachAsyncStreamImplOrderedAsync<TResult, TIn>(collection, func, maxBatchSizeToUse, estimatedResultSize, cancellationToken);
+            }
+        }
+
+        private static async IAsyncEnumerable<TResult> ForEachAsyncStreamImplUnbatchedAsync<TResult, TIn>(IAsyncEnumerable<TIn> collection, Func<TIn, CancellationToken, Task<TResult>> func, int estimatedResultSize, CancellationToken cancellationToken)
+        {
+            long runId = ParallelAsyncEventSource.Log.GetRunId();
+            ParallelAsyncEventSource.Log.RunStart(runId, 1, false, estimatedResultSize);
+
+            var enumerator = collection.GetAsyncEnumerator(cancellationToken);
+            try
+            {
+                var hasNext = true;
+                int batchId = 0;
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    hasNext = await enumerator.MoveNextAsync().ConfigureAwait(false);
+
+                    if (!hasNext)
+                    {
+                        break;
+                    }
+
+                    ParallelAsyncEventSource.Log.BatchStart(runId, batchId, 1);
+
+                    var element = enumerator.Current;
+                    var resultElement = await func(element, cancellationToken).ConfigureAwait(false);
+
+                    ParallelAsyncEventSource.Log.BatchStop(runId, batchId);
+
+                    batchId++;
+
+                    yield return resultElement;
+                }
+            }
+            finally
+            {
+                await enumerator.DisposeAsync();
+            }
+
+            ParallelAsyncEventSource.Log.RunStop(runId);
+        }
+
+        #endregion IAsyncEnumerable<T>
+    }
+
+#endif //NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+}
---
+++ tests/CSRakowski.Parallel.Benchmarks/Benchmarks/ParallelAsyncBenchmarks_AsyncStreams.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using CSRakowski.Parallel.Helpers;
+using CSRakowski.AsyncStreamsPreparations;
+
+namespace CSRakowski.Parallel.Benchmarks
+{
+    [MemoryDiagnoser]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
+    [SimpleJob(RuntimeMoniker.Net48, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: false)]
+    [SimpleJob(RuntimeMoniker.Net50, baseline: false)]
+    public class ParallelAsyncBenchmarks_AsyncStreams
+    {
+        private const int NumberOfItemsInCollection = 10000;
+
+        private readonly IEnumerable<int> InputNumbers;
+        private readonly IAsyncEnumerable<int> InputNumbersAsync;
+
+        public ParallelAsyncBenchmarks_AsyncStreams()
+        {
+            InputNumbers = Enumerable.Range(0, NumberOfItemsInCollection).ToList();
+            InputNumbersAsync = InputNumbers.AsAsyncEnumerable();
+        }
+
+        //[Params(1, 4, 8)]
+        [Params(1)]
+        public int MaxBatchSize { get; set; }
+
+        [Params(false, true)]
+        public bool AllowOutOfOrder { get; set; }
+
+        [Benchmark(Baseline = true), BenchmarkCategory("ForEachAsync", "IEnumerable")]
+        public async Task<int> IEnumerable_ForEachAsync()
+        {
+            var result = await ParallelAsync.ForEachAsync(InputNumbers, TestFunctions.JustAddOne_WithCancellationToken, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None);
+            return result.Count();
+        }
+
+        [Benchmark, BenchmarkCategory("ForEachAsync", "IAsyncEnumerable")]
+        public async Task<int> IAsyncEnumerable_ForEachAsync()
+        {
+            var result = await ParallelAsync.ForEachAsync(InputNumbersAsync, TestFunctions.JustAddOne_WithCancellationToken, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None);
+            return result.Count();
+        }
+
+        #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+
+        [Benchmark(Baseline = true), BenchmarkCategory("ForEachAsyncStream", "IEnumerable")]
+        public async Task<int> IEnumerable_ForEachAsyncStream()
+        {
+            int count = 0;
+
+            await foreach (var r in ParallelAsync.ForEachAsyncStream(InputNumbers, TestFunctions.JustAddOne_WithCancellationToken, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None))
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        [Benchmark, BenchmarkCategory("ForEachAsyncStream", "IAsyncEnumerable")]
+        public async Task<int> IAsyncEnumerable_ForEachAsyncStream()
+        {
+            int count = 0;
+
+            await foreach (var r in ParallelAsync.ForEachAsyncStream(InputNumbersAsync, TestFunctions.JustAddOne_WithCancellationToken, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None))
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        #endif //NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+
+    }
+}
