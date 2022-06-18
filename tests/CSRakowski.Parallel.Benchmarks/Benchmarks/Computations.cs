02de7d5ad16bd45a8ecdc6d91144053b7437d7ec
Christiaan Rakowski <cs.rakowski@gmail.com>
2022-06-18 19:32:27 +0200

    Added Compute based benchmark

---
+++ tests/CSRakowski.Parallel.Benchmarks/Benchmarks/Computations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace CSRakowski.Parallel.Benchmarks
+{
+    [MemoryDiagnoser]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    [CategoriesColumn]
+    //[SimpleJob(RuntimeMoniker.Net48, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: false)]
+    [SimpleJob(RuntimeMoniker.Net50, baseline: true)]
+    [SimpleJob(RuntimeMoniker.Net60, baseline: false)]
+    public class Computations
+    {
+        private const int NumberOfItemsInCollection = 10000;
+
+        private readonly List<int> InputNumbers;
+
+        public Computations()
+        {
+            InputNumbers = Enumerable.Range(0, NumberOfItemsInCollection).ToList();
+        }
+
+        [Params(1, 4, 8)]
+        public int MaxBatchSize { get; set; }
+
+        [Params(false, true)]
+        public bool AllowOutOfOrder { get; set; }
+
+        [Benchmark, BenchmarkCategory("Compute_Double")]
+        public Task Compute_Double()
+        {
+            return ParallelAsync.ForEachAsync(InputNumbers, TestFunctions.Compute_Double, MaxBatchSize, AllowOutOfOrder, NumberOfItemsInCollection, CancellationToken.None);
+        }
+
+        [Benchmark(Baseline = true), BenchmarkCategory("ReturnCompletedTask")]
+        public Task ReturnCompletedTask()
+        {
+            return ParallelAsync.ForEachAsync(InputNumbers, TestFunctions.ReturnCompletedTask, MaxBatchSize, AllowOutOfOrder, CancellationToken.None);
+        }
+    }
+}
